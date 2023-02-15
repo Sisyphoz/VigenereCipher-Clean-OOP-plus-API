526e44fa8a4af78d8f4900eb50b13182ec8de4f0
Alexander R <sisyphoz91@gmail.com>
2023-02-15 18:46:49 +0100

    Create VigenereCipherAPI.py

---
+++ VigenereCipherAPI.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from VigenereCipher import VigenereCipher
+
+app = Flask(__name__)
+
+# Initialize a new VigenereCipher object
+vc = VigenereCipher()
+
+# Endpoint for encrypting a plaintext with a given key
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    data = request.get_json()
+    plaintext = data['plaintext']
+    key = data['key']
+    ciphertext = vc.encrypt(plaintext, key)
+    return jsonify({'ciphertext': ciphertext})
+
+# Endpoint for decrypting a ciphertext with a given key
+@app.route('/decrypt', methods=['POST'])
+def decrypt():
+    data = request.get_json()
+    ciphertext = data['ciphertext']
+    key = data['key']
+    plaintext = vc.decrypt(ciphertext, key)
+    return jsonify({'plaintext': plaintext})
+
+if __name__ == '__main__':
+    app.run(debug=True)
